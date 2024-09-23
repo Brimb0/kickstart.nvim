de10e3037bbac84b66b7dd8149c8d4e25de845c4
Brimb0 <courchadaniel@gmail.com>
2024-09-23 22:36:19 +0800

    TODO: Finish Navigation.lua

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -3,6 +3,7 @@ return {
     'craftzdog/solarized-osaka.nvim',
     lazy = false,
     priority = 1000,
+    enabled = true,
     config = function()
       require('solarized-osaka').setup {
         transparent = true,
@@ -16,12 +17,48 @@ return {
           floats = 'transparent',
         },
         sidebars = { 'qf', 'help' },
-        day_brightness = 1,
-        hide_inactive_statusLine = true,
-        dim_inactive = false,
+        day_brightness = 1.0,
+        hide_inactive_statusLine = false,
+        dim_inactive = true,
         lualine_bold = true,
       }
       vim.cmd [[colorscheme solarized-osaka]]
     end,
+  },
+
+  {
+    {
+      'diegoulloao/neofusion.nvim',
+      lazy = false,
+      priority = 1000,
+      enabled = false,
+      config = function()
+        require('neofusion').setup {
+          terminal_colors = true, -- add neovim terminal colors
+          undercurl = true,
+          underline = true,
+          bold = true,
+          italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+          },
+          strikethrough = true,
+          invert_selection = false,
+          invert_signs = false,
+          invert_tabline = false,
+          invert_intend_guides = false,
+          inverse = true, -- invert background for search, diffs, statuslines and errors
+          palette_overrides = {},
+          overrides = {},
+          dim_inactive = false,
+          transparent_mode = true,
+        }
+
+        vim.cmd [[ colorscheme neofusion ]]
+      end,
+    },
   },
 }
